ff5de12371a101d46a42fd13970ff99fcaccdd8a
JulianCastillo14 <juliancast140502@gmail.com>
2023-12-09 11:57:53 -0500

    Accediendo a la galeria del celular

--- app/src/main/java/com/opar/opar/AgregarImagenesActivity.java
+++ app/src/main/java/com/opar/opar/AgregarImagenesActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AgregarImagenesActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGES = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,22 @@ public class AgregarImagenesActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String idInmueble = intent.getStringExtra("ID_INMUEBLE");
+
+        //Para acceder a la galeria
+
+        ImageView imageView = findViewById(R.id.imageView12);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGES);
+            }
+        });
+
+
+
     }
 }
