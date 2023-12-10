b7f709c77880cb1a79cb70aa1b193154f0fb7bc6
JulianCastillo14 <juliancast140502@gmail.com>
2023-12-10 12:02:55 -0500

    agregando inmueble arrendador activty

---
+++ app/src/main/java/com/opar/opar/InmuebleArrendadorActivty.java
@@ -0,0 +1,14 @@
+package com.opar.opar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InmuebleArrendadorActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inmueble_arrendador_activty);
+    }
+}
